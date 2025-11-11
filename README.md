9c10364727bb438849d0819881b27420aaad6345
Nicolas Quintana <nicolasquintana94@outlook.es>
2025-11-11 11:18:07 -0300

    RestServer & PostgreSql

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 #dev
 
 1. Clonar .env.template y crear el .env
+2. Ejecutar el comando ''docker compose up -d'' para ejecutar la base de datos
--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
 
 const todos = [
   { id: 1, text: 'Buy milk', completedAt: new Date() },
@@ -13,61 +15,77 @@ export class  TodosController {
 
   }
 
-  public getTodos = (req: Request, res: Response) => {
-    return res.json(todos);
+  public getTodos = async (req: Request, res: Response) => {
+    const todos = await prisma.todo.findMany();
+    res.json(todos);
   }
 
-  public getTodoById = (req: Request, res: Response) => {
+  public getTodoById = async (req: Request, res: Response) => {
     const id = +req.params.id;
 
     if(isNaN(id)) return res.status(400).json({error: 'Invalid id'});
 
-    const todo = todos.find(todo => todo.id === id);
+    const todo = await prisma.todo.findUnique({
+      where: {
+        id
+      }
+    });
 
-    (todo) ? res.json(todo) : res.status(404).json({error: 'Todo not found'});
-  }
-
-  public createTodo = (req: Request, res: Response) => {
-    const { text } = req.body;
-    if ( !text ) return res.status(400).json({error: 'Text is required'});
-
-    const newTodo = {
-      id: todos.length + 1,
-      text,
-      completedAt: new Date()
-    }
-    todos.push( newTodo );
-
-    res.json( newTodo );
-  }
-
-  public updateTodo = (req: Request, res: Response) => {
-    const  id  = +req.params.id;
-    if(isNaN(id)) return res.status(400).json({error: 'Invalid id'});
-
-    const todo = todos.find(todo => todo.id === id);
-    if( !todo ) return res.status(404).json({error: `Todo with id ${id} not found`});
-
-    const  { text, completedAt } = req.body;
-    if( !text ) return res.status(400).json({error: 'Text is required'});
-    todo.text = text || todo.text;
-    (completedAt === 'null') 
-    ? todo.completedAt = null 
-    : todo.completedAt = new Date( completedAt || todo.completedAt );
+    if( !todo ) return res.status(404).json({error: 'Todo not found'});
 
     res.json(todo);
   }
 
-  public deleteTodo = (req: Request, res: Response) => {
+  public createTodo = async (req: Request, res: Response) => {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
+    if( error ) return res.status(400).json({ error });
+
+    const todo = await prisma.todo.create({
+      data: createTodoDto!
+    });
+
+    res.json( todo );
+  }
+
+  public updateTodo = async (req: Request, res: Response) => {
+    const  id  = +req.params.id;
+    const [ error, updateTodoDto ] = UpdateTodoDto.create({ id, ...req.body });
+    if( error ) return res.status(400).json({ error });
+
+    const todo = await prisma.todo.findUnique({
+      where: { id }
+    });
+
+    if( !todo ) return res.status(404).json({error: `Todo with id ${id} not found`});
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: updateTodoDto!.values
+    });
+
+    res.json( updatedTodo );
+  }
+
+  public deleteTodo = async (req: Request, res: Response) => {
     const id = +req.params.id;
     if(isNaN(id)) return res.status(400).json({error: 'Invalid id'});
 
-    const todo = todos.find(todo => todo.id === id);
+    const todo = await prisma.todo.findUnique({
+      where: { id }
+    });
+
     if( !todo ) return res.status(404).json({error: `Todo with id ${id} not found`});
 
-    todos.splice(todos.indexOf(todo), 1);
+    const deletedTodo = await prisma.todo.delete({
+      where: { id }
+    });
 
-    res.json({message: 'Todo deleted successfully'});
+    (deletedTodo)
+    ? res.json({ deletedTodo })
+    : res.status(404).json({error: `Todo with id ${id} not found`});
+
   }
 
 }
+
+export default TodosController;
